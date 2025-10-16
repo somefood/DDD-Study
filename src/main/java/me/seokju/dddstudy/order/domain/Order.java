88517253320f0e6b7ab4bb506533dd05ddd559c9
somefood <iamseokjuhong@gmail.com>
2025-10-16 23:17:18 +0900

    Order 도메인 작성

---
+++ src/main/java/me/seokju/dddstudy/order/domain/Order.java
@@ -0,0 +1,12 @@
+package me.seokju.dddstudy.order.domain;
+
+public class Order {
+
+    public void changeShipped() {}
+
+    public void changeShippingInfo() {}
+
+    public void cancel() {}
+
+    public void completePayment() {}
+}
