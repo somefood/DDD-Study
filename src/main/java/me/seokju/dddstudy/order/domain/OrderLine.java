c8d6259bb5e23ca1c8a57c7268d56de02e062048
somefood <iamseokjuhong@gmail.com>
2025-10-16 23:19:36 +0900

    OrderLine 작성
    - 한 상품을 한 개 이상 주문할 수 있음
    - 각 상품의 구매 가격 합은 상품 가격에 구매 개수를 곱한 값

---
+++ src/main/java/me/seokju/dddstudy/order/domain/OrderLine.java
@@ -0,0 +1,25 @@
+package me.seokju.dddstudy.order.domain;
+
+import lombok.Getter;
+
+@Getter
+public class OrderLine {
+    private Product product;
+
+    private int price;
+
+    private int quantity;
+
+    private int amounts;
+
+    public OrderLine(Product product, int price, int quantity, int amounts) {
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+        this.amounts = amounts;
+    }
+
+    private int calculateAmounts() {
+        return price * quantity;
+    }
+}
