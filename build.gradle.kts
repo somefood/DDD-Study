8a43fd8f38927e99e8e24a55e2a6f3cfc192e524
somefood <iamseokjuhong@gmail.com>
2025-10-16 23:09:03 +0900

    git init

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.6"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.hibernate.orm") version "6.6.29.Final"
+    id("org.graalvm.buildtools.native") version "0.10.6"
+}
+
+group = "me.seokju"
+version = "0.0.1-SNAPSHOT"
+description = "DDD-study"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+hibernate {
+    enhancement {
+        enableAssociationManagement = true
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/me/seokju/dddstudy/DddStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.seokju.dddstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
